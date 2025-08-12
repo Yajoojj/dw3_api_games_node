c33a80b3d689231d173d3bc51dae2c5a05e08b39
Yajoojj <yaokurashiki@outlook.com>
2025-08-12 20:33:45 -0300

    primeiro commit

---
+++ 01-criacao-da-api/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+const app = express();
+
+// Configuração do EXPRESS
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// Criando um retorno da API
+app.get("/", (req, res) => {
+    const games = [
+        {
+            tittle: "Delta",
+            year: 2024,
+            genre: "FPS",
+            plataform: "PC (Windows)",
+            price: 0
+        },
+        {
+            tittle: "Diablo III",
+            year: 2009,
+            genre: "RPG",
+            plataform: "PC (Windows)",
+            price: 150
+        },
+        {
+            tittle: "League of Legends",
+            year: 2009,
+            genre: "MOBA",
+            plataform: "PC (Windows)",
+            price: 0
+        }
+    ];
+    res.json(games);
+})
+
+// Rodando a API na porta 4000
+const port = 4000;
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error)
+    }
+    console.log(`API rodando em http:/localhost:${port}`);
+});
