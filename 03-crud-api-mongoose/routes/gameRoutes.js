662601ab664410e5851e5409655071f9423a6d39
Yago Uran Kurashiki Rios <145205598+Yajoojj@users.noreply.github.com>
2025-08-28 19:34:11 -0300

    Delete 03-crud-api-mongoose directory

--- 03-crud-api-mongoose/routes/gameRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const gameRoutes = express.Router()
-import gameController from '../controllers/gameController.js'
-
-// a camada de routes será responsável por definir as rotas da aplicação
-gameRoutes.get("/games", gameController.getAllGames)
-
-export default gameRoutes;
