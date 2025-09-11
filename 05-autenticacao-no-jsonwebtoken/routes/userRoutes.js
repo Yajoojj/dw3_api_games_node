d36dac91c28920bbdaacb79f642940768e58b0b3
Yago Uran Kurashiki Rios <145205598+Yajoojj@users.noreply.github.com>
2025-09-10 22:22:18 -0300

    Delete 05-autenticacao-no-jsonwebtoken directory

--- 05-autenticacao-no-jsonwebtoken/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-const userRoutes = express.Router();
-import userController from "../controllers/userController.js"
-
-// Rota para CADASTRAR um usuario
-userRoutes.post("/user", userController.createUser);
-
-export default userRoutes;
